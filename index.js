6e9402e989d6661b6ac7750d2f9be0876d078fb6
Oliwrm <89049255+Oliwrm@users.noreply.github.com>
2023-12-12 17:53:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function updateBrightness(brightness) {
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api.thingspeak.com/update?api_key=XUH1IVZHH7DH791Q&field3=" + brightness, true);
     xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = () => {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        const data = xhr.response;
+        console.log(data);
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
+
     
 }document.getElementById('updateButton').addEventListener('click', function() {
     var brightness = document.getElementById('brightnessSlider').value;
