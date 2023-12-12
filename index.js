3bd2079264dbf179afe87cc3421a53e520f816e7
Oliwrm <89049255+Oliwrm@users.noreply.github.com>
2023-12-12 02:17:12 +0000

    second commit

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById('updateButton').addEventListener('click', function() {
+    var brightness = document.getElementById('brightnessSlider').value;
+    updateBrightness(brightness);
+});
+
+function updateBrightness(brightness) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.thingspeak.com/update?api_key=XUH1IVZHH7DH791Q&field2=" + brightness, true);
+    xhr.send();
+}
