47bb9870c864f1e2df082dc9d31690279be11c60
Oliwrm <89049255+Oliwrm@users.noreply.github.com>
2023-12-12 18:43:12 +0000

    final

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 function updateBrightness(brightness) {
-    var xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api.thingspeak.com/update?api_key=XUH1IVZHH7DH791Q&field3=" + brightness, true);
-    xhr.send();
     
-}document.getElementById('updateButton').addEventListener('click', function() {
+    // Set the responseType after calling open() and before send()
+    xhr.responseType = "json";
+    
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // Since the responseType is set to "json", you can directly access xhr.response
+            const data = xhr.response;
+            console.log(data);
+        } else {
+            console.log(`Error: ${xhr.status}`);
+        }
+    };
+
+    // Send the request after setting up the onload callback
+    xhr.send();
+}
+
+document.getElementById('updateButton').addEventListener('click', function() {
     var brightness = document.getElementById('brightnessSlider').value;
     updateBrightness(brightness);
 });
-
-
